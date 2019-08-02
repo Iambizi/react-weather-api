71e3e821f3a38811d7ee0f2fd9f3c16ffe222c8f
Amir Bizimana <amirbzmn@gmail.com>
2019-08-02 09:56:32 -0400

    form component functionality

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ import axios from "axios";
 const API_KEY = "d3b1e8e765544cb816a08a596dbd3fbe";
 
 class App extends Component {
-  
-  async componentDidMount() {
+  async getWeather(e) {
     const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}`;
 
     const response = await axios.get(url);
 
-    console.log(response);
+    console.log(response.data);
+  }
+
+  async componentDidMount() {
+    this.getWeather();
   }
 
   render() {
     return (
       <React.Fragment>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </React.Fragment>
     );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,16 @@ class Form extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>Form Component</div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            this.props.getWeather();
+          }}
+        >
+          <input type="text" name="city" placeholder="City..." />
+          <input type="text" name="country" placeholder="Country..." />
+          <button>Get Weather</button>
+        </form>
       </React.Fragment>
     );
   }
