a051254856582656c935811f4d704a3e92280cfb
Amir Bizimana <amirbzmn@gmail.com>
2019-08-06 19:53:50 -0400

    no state in weather component so converted to sfc

--- src/App.js
+++ src/App.js
@@ -37,16 +37,27 @@ class App extends Component {
       `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      console.log(data);
 
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter values for city and country."
+      });
+    }
   };
 
   render() {
@@ -60,6 +71,7 @@ class App extends Component {
           country={this.state.country}
           humidity={this.state.humidity}
           description={this.state.description}
+          error={this.state.error}
         />
       </React.Fragment>
     );
