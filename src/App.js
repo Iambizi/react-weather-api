8d8c442c99e9bea78f405d48ac7ae433f5f9fbad
Amir Bizimana <amirbzmn@gmail.com>
2019-08-06 19:13:24 -0400

    props passed to weather component and logic set up for each piece of state

--- src/App.js
+++ src/App.js
@@ -20,6 +20,14 @@ class App extends Component {
   // componentDidMount() {
   //   this.getWeather();
   // }
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
   getWeather = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -29,8 +37,16 @@ class App extends Component {
       `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
-
     console.log(data);
+
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   };
 
   render() {
@@ -38,7 +54,13 @@ class App extends Component {
       <React.Fragment>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+        />
       </React.Fragment>
     );
   }
