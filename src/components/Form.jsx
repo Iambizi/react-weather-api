28ff3cb15284463c3f2c91acfbfd5bafa1761706
Amir Bizimana <amirbzmn@gmail.com>
2019-08-06 20:02:53 -0400

    changed Form and Titles component to sfc

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Form extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <form
-          // onSubmit={e => {
-          //   e.preventDefault();
-          //   this.props.getWeather();
-          // }}
-          onSubmit={this.props.getWeather}
-        >
-          <input type="text" name="city" placeholder="City..." />
-          <input type="text" name="country" placeholder="Country..." />
-          <button>Get Weather</button>
-        </form>
-      </React.Fragment>
-    );
-  }
-}
+const Form = props => (
+  <React.Fragment>
+    <form
+      // onSubmit={e => {
+      //   e.preventDefault();
+      //   this.props.getWeather();
+      // }}
+      onSubmit={props.getWeather}
+    >
+      <input type="text" name="city" placeholder="City..." />
+      <input type="text" name="country" placeholder="Country..." />
+      <button>Get Weather</button>
+    </form>
+  </React.Fragment>
+);
 
 export default Form;
--- src/components/Titles.jsx
+++ src/components/Titles.jsx
@@ -1,15 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Titles extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
-      </React.Fragment>
-    );
-  }
-}
+const Titles = () => (
+  <React.Fragment>
+    <h1>Weather Finder</h1>
+    <p>Find out temperature, conditions and more...</p>
+  </React.Fragment>
+);
 
 export default Titles;
